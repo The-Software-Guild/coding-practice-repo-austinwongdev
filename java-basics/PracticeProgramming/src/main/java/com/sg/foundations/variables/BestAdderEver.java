16a753733ff87d46bbb2c54324bbf75a89ac8f7c
Austin Wong <austinwongdev@gmail.com>
2021-07-26 16:03:27 -0700

    Completed Practice Programming #2 - Variables

---
+++ java-basics/PracticeProgramming/src/main/java/com/sg/foundations/variables/BestAdderEver.java
@@ -0,0 +1,24 @@
+/*
+ * @author Austin Wong
+ * email: austinwongdev@gmail.com
+ * date: Jul 26, 2021
+ * purpose: To practice doing basic math with variables
+ */
+
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author Austin Wong
+ */
+public class BestAdderEver {
+    public static void main(String[] args){
+        int num1 = 7;
+        int num2 = 23;
+        int num3 = 2;
+        
+        int sum = num1 + num2 + num3;
+        
+        System.out.println(num1 + " + " + num2 + " + " + num3 + " = " + sum);
+    }
+}
---
+++ java-basics/PracticeProgramming/src/main/java/com/sg/foundations/variables/InABucket.java
@@ -0,0 +1,29 @@
+/*
+ * @author Austin Wong
+ * email: austinwongdev@gmail.com
+ * date: July 26, 2021
+ * purpose: To practice assigning values to variables
+ */
+
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author Austin Wong
+ */
+public class InABucket {
+    public static void main(String[] args){
+        
+        String walrus;
+        double piesEaten;
+        float weightOfTeacupPig;
+        int grainsOfSand;
+        
+        walrus = "Sir Leroy Jenkins III";
+        piesEaten = 42.1;
+        
+        System.out.println("Meet my pet walrus, " + walrus);
+        System.out.print("He was a bit hungry today, and ate this many pies : ");
+        System.out.println(piesEaten);
+    }
+}
