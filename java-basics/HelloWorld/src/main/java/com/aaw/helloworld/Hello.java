88bc38a566fe82ae48f60486ee1b3e9a7c88cf4d
Austin Wong <austinwongdev@gmail.com>
2021-07-26 12:11:19 -0700

    Hello World in NetBeans

---
+++ java-basics/HelloWorld/src/main/java/com/aaw/helloworld/Hello.java
@@ -0,0 +1,14 @@
+/**
+ *@author Austin Wong
+ *email: austinwongdev@gmail.com
+ *date: 7/26/21
+ *purpose: My first Hello World program in an IDE
+ */
+
+package com.aaw.helloworld; 
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Austin!");
+    }
+}
