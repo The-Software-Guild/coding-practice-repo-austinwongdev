66d7dab26e5853478b5cbfeee4f70924dc03b055
Austin Wong <austinwongdev@gmail.com>
2021-08-02 08:30:07 -0700

    Modified ClassRosterServiceLayerImpl to validate input for new students

--- classes-and-objects/ClassRoster/src/main/java/com/sg/classroster/service/ClassRosterServiceLayerImpl.java
+++ classes-and-objects/ClassRoster/src/main/java/com/sg/classroster/service/ClassRosterServiceLayerImpl.java
@@ -7,6 +7,7 @@
 
 package com.sg.classroster.service;
 
+import com.sg.classroster.dao.ClassRosterDao;
 import com.sg.classroster.dao.ClassRosterPersistenceException;
 import com.sg.classroster.dto.Student;
 import java.util.List;
@@ -17,27 +18,62 @@ import java.util.List;
  */
 public class ClassRosterServiceLayerImpl implements ClassRosterServiceLayer {
 
+    ClassRosterDao dao;
+    
+    public ClassRosterServiceLayerImpl(ClassRosterDao dao){
+        this.dao = dao;
+    }
+    
     @Override
     public void createStudent(Student student) throws
             ClassRosterDuplicateIdException,
             ClassRosterDataValidationException,
             ClassRosterPersistenceException{
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        if (dao.getStudent(student.getStudentId()) != null){
+            throw new ClassRosterDuplicateIdException(
+                    "ERROR: Could not create student. Student Id "
+                    + student.getStudentId()
+                    + " already exists");
+        }
+        
+        validateStudentData(student);
+        
+        dao.addStudent(student.getStudentId(), student);
     }
     
+    @Override
     public List<Student> getAllStudents() throws
             ClassRosterPersistenceException{
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        return dao.getAllStudents();
     }
     
+    @Override
     public Student getStudent(String studentId) throws
             ClassRosterPersistenceException{
-        throw new UnsupportedOperationException("Not supported yet.");
+        return dao.getStudent(studentId);
     }
     
+    @Override
     public Student removeStudent(String studentId) throws
             ClassRosterPersistenceException{
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        return dao.removeStudent(studentId);
     }
     
+    private void validateStudentData(Student student) throws
+            ClassRosterDataValidationException{
+        
+        if (student.getFirstName() == null
+                || student.getFirstName().trim().length() == 0
+                || student.getLastName() == null
+                || student.getLastName().trim().length() == 0
+                || student.getCohort() == null
+                || student.getCohort().trim().length() == 0){
+            
+            throw new ClassRosterDataValidationException(
+                    "ERROR: All fields [First Name, Last Name, Cohort] are required.");
+        }
+    }
 }
